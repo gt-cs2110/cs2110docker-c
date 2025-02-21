43512b3122d2e06a9782559b019f9bee99935439
Henry Bui <henry.h.bui05@gmail.com>
2025-02-21 12:19:11 -0500

    Add git to stable

--- src/install/base/tools.sh
+++ src/install/base/tools.sh
@@ -5,6 +5,9 @@ set -e
 echo "Install some common tools for further installation"
 apt-get update
 apt-get install -y wget net-tools locales bzip2 zip unzip
+# For people working in Git repositories in a dev container,
+# (e.g., AG devs for C or superuser students)
+apt-get install -y git
 apt-get clean -y
 
 echo "generate locales für en_US.UTF-8"
